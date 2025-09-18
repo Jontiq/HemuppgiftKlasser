76b5955a1df8f8c1066fa213974ed4a22328bf89
Jontiq <99jonathancar@gmail.com>
2025-09-18 13:03:58 +0200

    Add project files.

---
+++ HemuppgiftKlasser/Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HemuppgiftKlasser
+{
+    internal class Employee
+    {
+        //Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        private double _salary;
+        public double Salary
+        {
+            get
+            {
+                return _salary;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    _salary = 0;
+                }
+                else
+                {
+                    _salary = value;
+                }
+            }
+        }
+
+        //Konstruktor
+        public Employee(string FirstName, string LastName)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+
+        //Metoder
+        //Returnerar årslön
+        public double GetAnnualSalary()
+        {
+            return Salary * 12;
+        }
+
+        public void GiveRaise(double percentage)
+        {
+            if (percentage > 0)
+            {
+                Salary *= (1+(percentage/100));
+            }
+        }
+    }
+}
---
+++ HemuppgiftKlasser/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HemuppgiftKlasser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Jane", "Smith");
+            employee.Salary = 3000;
+            Console.WriteLine($"Annual Salary: {employee.GetAnnualSalary()}");
+            employee.GiveRaise(10);
+            Console.WriteLine($"New Monthly Salary: {employee.Salary}");
+            Console.WriteLine($"New Annual Salary: {employee.GetAnnualSalary()}");
+
+
+        }
+    }
+}
